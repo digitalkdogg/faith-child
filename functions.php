86c7467800e418b83725dc3a9ce509f4ba5850a4
Kevin Bollman <digitalkdogg@gmail.com>
2021-10-24 13:29:03 -0500

    commiting functions to see if I can get routing to work on server

--- functions.php
+++ functions.php
@@ -25,18 +25,18 @@ if ( ! function_exists( 'suffice_child_enqueue_child_styles' ) ) {
 		wp_enqueue_script( 'customjs', get_stylesheet_directory_uri() . '/assets/millwood.js' );
 
 		$php_vars = array();
-		try {$php_vars['stylesheet_dir'] = get_stylesheet_directory_uri();} catch (exception $e) {}
-		try {$php_vars['home_url'] = get_home_url();} catch (exception $e) {}
-		try {$php_vars['site_url'] = get_option( 'siteurl' );} catch (exception $e) {}
-		try {$php_vars['is_admin'] = is_admin();} catch (exception $e) {}
-		try {$php_vars['menu'] = wp_get_nav_menu_items('MainMenu');} catch (exception $e) {}
+		try {$php_vars['stylesheet_dir'] = get_stylesheet_directory_uri(); } catch (excetpion $e) {}
+		try {$php_vars['home_url'] = get_home_url(); } catch (exception $e) {} 
+		try {$php_vars['site_url'] = get_option( 'siteurl' ); } catch (exception $e) {}
+		try {$php_vars['is_admin'] = is_admin(); } catch(exception $e) {}
+		try {$php_vars['menu'] = wp_get_nav_menu_items('MainMenu'); } catch(exception $e) {}
+		try {$php_vars['rest_url'] = get_rest_url();} catch(exception $e) {}
 		
-		try {$php_vars['templates']['stripe'] = get_custom_template_file('template-parts/stripe.html');} catch (exception $e) {} 
+		try { $php_vars['templates']['stripe'] = get_custom_template_file('template-parts/stripe.html'); } catch(exception $e) {}
 
-		try {$php_vars['stripe_api_test_key'] = $all_settings['stripe_test_key_text_block'];} catch (exception $e) {}
-		try {$php_vars['use_test_data'] = $all_settings['use_test_data'];} catch (exception $e) {}
-
-
+		if (isset($all_settings['stripe_test_key_text_block'])) : $php_vars['stripe_api_test_key'] = $all_settings['stripe_test_key_text_block']; endif;
+		if (isset($all_settings['use_test_data'])) : $php_vars['use_test_data'] = $all_settings['use_test_data']; endif;
+		if (isset($all_settings['stripe_live_key_text_block'])) : $php_vars['stripe_api_live_key'] = $all_settings['stripe_api_live_key']; endif;
 
 		wp_add_inline_script('customjs', 'var php_vars = ' . wp_json_encode($php_vars));
 	 }
@@ -50,6 +50,56 @@ function get_custom_template_file($fileName){
     $fileContents = file_get_contents($filePath);
     return $fileContents;
 }
+
+  /**
+* Generates new stripe intent
+* @param array $data Options for the function.
+* @return $object, or null if none.  */
+
+ function get_stripe_intent ( $params ){
+
+    require_once(get_home_path(). 'stripe-php/init.php');
+
+
+    if ($params['type']=='live') {
+      $stripe = new \Stripe\StripeClient(
+        pastore_church_get_custom_option('api_stripe_payment_live_secret')
+      );
+    } else {
+      $stripe = new \Stripe\StripeClient(
+        pastore_church_get_custom_option('api_stripe_payment_test_secret')
+      );
+    }
+
+      $customer = $stripe->customers->create([
+        'description' => $params['name'],
+        'email' =>$params['email'],
+        'address' => $params['address'],
+      ]);
+
+      $response = $stripe->paymentIntents->create([
+        'amount' => $params['amount'],
+        'currency' => 'usd',
+        'receipt_email'=>$params['email'],
+        'description'=>$params['name'],
+        'customer'=>$customer->id,
+        'payment_method_types' => ['card'],
+      ]);
+
+      $response['customer'] =$customer;
+
+       return $response;
+
+ }
+
+
+	 // Register the rest route here.
+  	 add_action( 'rest_api_init', function () {
+            register_rest_route('stripe/v1', 'create_intent', array(
+              'methods' => 'POST',
+              'callback' => 'get_stripe_intent'
+            ));
+     } );
 
 //if (get_included_files()[0]) {
 include_once WP_CONTENT_DIR . '/themes/faith-child/inc/customizer.php';
